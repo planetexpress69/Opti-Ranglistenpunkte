dd13fb36e8b84c0fee587afc2cd96cb25d48cd11
Martin Kautz <kautz@jakota.de>
2014-12-01 11:46:26 +0100

    Added type checking and downcasting.

--- Ranglistenpunkte/RankingDetailViewControllerSwift.swift
+++ Ranglistenpunkte/RankingDetailViewControllerSwift.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class RankingDetailViewControllerSwift: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    @IBOutlet weak  var theTableView:UITableView!
+    @IBOutlet weak var theTableView:UITableView!
     var theDataSource = NSDictionary()
 
     // -----------------------------------------------------------------------------------------------------------------
@@ -18,8 +18,8 @@ class RankingDetailViewControllerSwift: UIViewController, UITableViewDataSource,
     // -----------------------------------------------------------------------------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
-        theTableView.delegate   = self
-        theTableView.dataSource = self;
+        self.theTableView.delegate   = self
+        self.theTableView.dataSource = self;
         self.title              = "Detail"
     }
 
@@ -42,11 +42,11 @@ class RankingDetailViewControllerSwift: UIViewController, UITableViewDataSource,
         case 0:
             return 1
         default:
-            if let regattas = theDataSource["regatta"] as? NSArray {
+            if let regattas = self.theDataSource["regatta"] as? NSArray {
                 var counter:Int = 0
                 for elem in regattas {
                     if let regatta = elem as? NSDictionary {
-                        if (regatta["regattaId"] != nil) {
+                        if let regattaId = regatta["regattaId"] as? String {
                             counter++
                         }
                     }
@@ -67,29 +67,103 @@ class RankingDetailViewControllerSwift: UIViewController, UITableViewDataSource,
             var cell:SailorCell = SailorCell()
             cell = tableView.dequeueReusableCellWithIdentifier("SailorCell", forIndexPath: indexPath) as SailorCell
             cell.selectionStyle = .None
-
-            cell.firstnamenameLabel.text    = (theDataSource["firstname"] as String) + " " + (theDataSource["name"] as String)
-            cell.clubLabel.text             = theDataSource["club"] as? String
-            cell.sailCountryLabel.text      = theDataSource["sailCountry"] as? String
-            cell.sailNumberLabel.text       = theDataSource["sailNumber"] as? String
-            cell.yobLabel.text              = theDataSource["yob"] as? String
-            cell.totalRunsLabel.text        = String(theDataSource["totalRuns"] as Int)
-            cell.totalPointsLabel.text      = NSString(format:"%.2f", theDataSource["totalPoints"] as Float)
-            cell.posLabel.text              = String(theDataSource["pos"] as Int)
+            // ---------------------------------------------------------------------------------------------------------
+            cell.firstnamenameLabel.text    = (self.theDataSource["firstname"] as String) + " " + (theDataSource["name"] as String)
+            cell.clubLabel.text             = self.theDataSource["club"] as? String
+            cell.sailCountryLabel.text      = self.theDataSource["sailCountry"] as? String
+            cell.sailNumberLabel.text       = self.theDataSource["sailNumber"] as? String
+            // ---------------------------------------------------------------------------------------------------------
+            var yob: String
+            if let y = self.theDataSource["yob"] as? String {
+                yob = y
+            } else if let y = self.theDataSource["yob"] as? Int {
+                yob = String(y)
+            } else {
+                yob = "-"
+            }
+            cell.yobLabel.text = yob
+            // ---------------------------------------------------------------------------------------------------------
+            var totalPoints: String
+            if let tp = self.theDataSource["totalPoints"] as? String {
+                totalPoints = tp
+            } else if let tp = self.theDataSource["totalPoints"] as? Float {
+                totalPoints = NSString(format:"%.2f", tp)
+            } else {
+                totalPoints = "-"
+            }
+            cell.totalPointsLabel.text = totalPoints
+            // ---------------------------------------------------------------------------------------------------------
+            var totalRuns : String
+            if let tr = self.theDataSource["totalRuns"] as? String {
+                totalRuns = tr
+            } else if let tr = self.theDataSource["totalRuns"] as? Int {
+                totalRuns = String(tr)
+            } else {
+                totalRuns = "-"
+            }
+            cell.totalRunsLabel.text = totalRuns
+            // ---------------------------------------------------------------------------------------------------------
+            var pos: String
+            if let p = self.theDataSource["pos"] as? String {
+                pos = p
+            } else if let p = self.theDataSource["pos"] as? Int {
+                pos = String(p)
+            } else {
+                pos = "-"
+            }
+            cell.posLabel.text = pos
+            // ---------------------------------------------------------------------------------------------------------
             return cell
 
         default:
-            let regatta = theDataSource["regatta"]?[indexPath.row] as NSDictionary
+            let regatta = self.theDataSource["regatta"]?[indexPath.row] as NSDictionary
             var cell:RegattaDetailCell = RegattaDetailCell()
             cell = tableView.dequeueReusableCellWithIdentifier("RegattaDetailCell", forIndexPath: indexPath) as RegattaDetailCell
             cell.selectionStyle = .None
-
+            // ---------------------------------------------------------------------------------------------------------
             cell.rnamelabel.text            = regatta["rname"] as? String
             cell.sl_pointsLabel.text        = regatta["sl_points"] as? String
             cell.sl_points_cupLabel.text    = regatta["sl_points_cup"] as? String
-            cell.positionboatsLabel.text    = (regatta["position"] as String) + "/" + (regatta["boats"] as String)
-            cell.runs_totalLabel.text       = regatta["runs_total"] as? String
-            cell.runs_scoredLabel.text      = regatta["runs_scored"] as? String
+            // ---------------------------------------------------------------------------------------------------------
+            var position : String
+            if let pos = regatta["position"] as? String {
+                position = pos
+            } else if let pos = regatta["position"] as? Int {
+                position = String(pos)
+            } else {
+                position = "-"
+            }
+            // ---------------------------------------------------------------------------------------------------------
+            var boats : String
+            if let b = regatta["boats"] as? String {
+                boats = b
+            } else if let b = regatta["boats"] as? Int {
+                boats = String(b)
+            } else {
+                boats = "-"
+            }
+            cell.positionboatsLabel.text    = position + "/" + boats
+            // ---------------------------------------------------------------------------------------------------------
+            var total: String
+            if let t: String = regatta["runs_total"] as? String {
+                total = t
+            } else if let t: Int = regatta["runs_total"] as? Int {
+                total = String(t)
+            } else {
+                total = "-"
+            }
+            cell.runs_totalLabel.text = total
+            // ---------------------------------------------------------------------------------------------------------
+            var scored: String
+            if let s: String = regatta["runs_scored"] as? String {
+                scored = s
+            } else if let s: Int = regatta["runs_scored"] as? Int {
+                scored = String(s)
+            } else {
+                scored = "-"
+            }
+            cell.runs_scoredLabel.text = scored
+            // ---------------------------------------------------------------------------------------------------------
             return cell
         }
     }
